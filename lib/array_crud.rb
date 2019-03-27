39777ab2360dadee3e457c8bb1353e4f6defa522
Ryan Ditzel <ryand370@gmail.com>
2019-03-27 22:25:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,12 +25,11 @@ end
 def retrieve_element_from_index(array, index_number)
   array[index_number]
 end
-retrieve_element_from_index(array, 2)
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
